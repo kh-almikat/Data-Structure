3a07498e287d14b3446fced3c4e6e0abbf88f748
Khondokar Almikat <khondoker.almikat@gmail.com>
2026-07-16 20:55:57 +0600

    Add files via upload

---
+++ Doubly Linked List/input.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+
+    public:
+    int val;
+    Node* next;
+    Node* prev;
+
+
+    Node(int val){
+
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+
+    }
+
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int val){
+
+    Node* newnode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+
+    tail->next = newnode;
+    newnode->prev = tail;
+    tail = newnode;
+}
+
+void print_forward(Node* head){
+
+    Node* temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    
+    cout << endl;
+}
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+
+        
+    /*
+    if last element isn't -1, input mathod
+
+    int val;
+    while(cin << val)
+    {
+        insert_at_tail(head,tail,val);
+    }
+    
+    */
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+        
+    }
+    print_forward(head);
+
+}
