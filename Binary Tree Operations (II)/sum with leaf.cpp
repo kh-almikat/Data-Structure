1779532c23da5b00c621419e45a9e590ad16027b
Khondokar Almikat <khondoker.almikat@gmail.com>
2026-08-17 21:35:24 +0600

    Add files via upload

---
+++ Binary Tree Operations (II)/sum with leaf.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node* left;
+    Node* right;
+
+    Node(int val)
+    {
+        this->val = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node* input()
+{
+    int val;
+    cin >> val;
+
+    Node *root;
+    if(val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+
+    queue<Node *> q;
+    if(root)
+        q.push(root);
+
+    while(!q.empty())
+    {
+        Node *f = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+
+        Node *myleft,*myright;
+
+        if(l == -1)
+            myleft = NULL;
+        else
+            myleft = new Node(l);
+
+        if(r == -1)
+            myright = NULL;
+        else
+            myright = new Node(r);
+
+        f->left = myleft;
+        f->right = myright;
+
+        if(f->left)
+            q.push(f->left);
+        if(f->right)
+            q.push(f->right);
+    }
+
+    return root;
+}
+
+int sum(Node* root)
+{
+    if(root == NULL)
+        return 0;
+
+    int l = sum(root->left);
+    int r = sum(root->right);
+
+    return l + r + root->val;
+}
+
+int main() 
+{
+    Node *root = input();
+    cout << sum(root) << endl;
+}
