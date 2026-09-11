b1ee66763cc68fecf605ec7bf6494a09f28d9b4b
Khondokar Almikat <khondoker.almikat@gmail.com>
2026-09-11 22:48:30 +0600

    Add files via upload

---
+++ Heap Implementation/insert max_heap.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int>v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    int val;
+    cin >> val;
+    v.push_back(val);
+
+    int cur_idx = v.size()-1;
+
+    while(cur_idx != 0)
+    {
+        int parent_idx = (cur_idx - 1)/2;
+
+        if (v[cur_idx] > v[parent_idx])
+        {
+            swap(v[cur_idx], v[parent_idx]);
+            cur_idx = parent_idx;
+        }
+        else
+            break;
+    }
+    for (int u : v)
+        cout << u << " ";
+    cout << endl;
+}
